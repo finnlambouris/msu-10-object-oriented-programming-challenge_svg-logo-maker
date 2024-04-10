6d34301624ed452100d4ed3ac4344a72be8efc70
finntendoverse <finnlambouris616@gmail.com>
2024-04-10 01:04:36 -0400

    added code comments

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// varibles for the required modules
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 // function to handle inquirer prompts
-handlePrompting = function() {
-    let getLogoText = async function() {                                                                                                                                        // WHEN the getProjectTitle function is called
-        let logoText = await inquirer.prompt(                                                                                                                                   // THEN the projectTitle will be saved when entered
+handlePrompting = function() {                                      // WHEN the handlePrompting function is called
+    let getLogoText = async function() {                                                                            // WHEN the getLogoText function is called
+        let logoText = await inquirer.prompt(                                                                       // THEN the logoText will be saved when entered
             [
                 {
                     name: 'logoText',
@@ -14,18 +15,18 @@ handlePrompting = function() {
             ]
         )
     
-        if (logoText.logoText.length > 3) {                                                                                                                                            // IF the project title is left empty
-            console.log('please try again by entering 3 characters or less.')
-            getLogoText();                                                                                                                             // THEN it will be titled "untitled project"
-        } else {
-            getTextColor(logoText)
-        }                                                                                                                                   // THEN the user is prompted to enter their project description
+        if (logoText.logoText.length > 3) {                                                                         // IF the logo text entered is more than 3 characters
+            console.log('please try again by entering 3 characters or less.')                                       // THEN the user will be asked to try again by entering 3 characters or less
+            getLogoText();                                                                                          // THEN the user will be prompted to enter their logo text
+        } else {                                                                                                    // IF the logo text entered is 3 characters or less
+            getTextColor(logoText)                                                                                  // THEN the user will be prompted to pick the color of their text
+        }
     }
 
-    getLogoText();
+    getLogoText();                                                  // THEN the user will be prompted to enter their logo text
     
-    let getTextColor = async function(logoText) {
-        let textColor = await inquirer.prompt(                                                                                                                                   // THEN the projectTitle will be saved when entered
+    let getTextColor = async function(logoText) {                                                                   // WHEN the getTextColor function is called
+        let textColor = await inquirer.prompt(                                                                      // THEN the textColor will be saved when entered
             [
                 {
                     name: 'textColor',
@@ -34,11 +35,11 @@ handlePrompting = function() {
                 }
             ]
         )
-        getLogoShape(logoText, textColor)
+        getLogoShape(logoText, textColor)                                                                           // THEN the user will be prompted to pick their logo's shape
     }
     
-    let getLogoShape = async function(logoText, textColor) {
-        let logoShape = await inquirer.prompt(
+    let getLogoShape = async function(logoText, textColor) {                                                        // WHEN the getLogoShape function is called
+        let logoShape = await inquirer.prompt(                                                                      // THEN the logoShape will be saved when entered
             [
                 {
                     name: 'logoShape',
@@ -48,11 +49,11 @@ handlePrompting = function() {
                 }
             ]
         )
-        getBackgroundColor(logoText, textColor, logoShape);
+        getShapeColor(logoText, textColor, logoShape);                                                              // THEN the user will be prompted to pick the shape's color
     }
     
-    let getBackgroundColor = async function(logoText, textColor, logoShape) {
-        let shapeColor = await inquirer.prompt(
+    let getShapeColor = async function(logoText, textColor, logoShape) {                                            // WHEN the getShapeColor function is called
+        let shapeColor = await inquirer.prompt(                                                                     // THEN the shapeColor will be saved when entered
             [
                 {
                     name: 'shapeColor',
@@ -61,37 +62,38 @@ handlePrompting = function() {
                 }
             ]
         )
-        createSVG(logoText, textColor, logoShape, shapeColor);
+        createSVG(logoText, textColor, logoShape, shapeColor);                                                      // THEN the SVG file will be created
     }
 }
 
 // function to handle creating the SVG file
-createSVG = function(logoText, textColor, logoShape, shapeColor) {
-    const Shapes = require('./lib/shapes.js');
-    const Circle = Shapes[0];
-    const Triangle = Shapes[1];
-    const Square = Shapes[2];
+createSVG = function(logoText, textColor, logoShape, shapeColor) {                                                              // WHEN the createSVG function is called
+    const Shapes = require('./lib/shapes.js');                                                                                  // THEN the shapes file module exports are referenced
+    const Circle = Shapes[0];                                                                                                   // THEN the Circle class in the shapes file is referenced
+    const Triangle = Shapes[1];                                                                                                 // THEN the Triangle class in the shapes file is referenced
+    const Square = Shapes[2];                                                                                                   // THEN the Square class in the shapes file is referenced
 
-    const userInputs = {
+    const userInputs = {                                                                                                        // THEN the userInputs from the handlePrompting function are stored in an object
         logoText: logoText.logoText,
         textColor: textColor.textColor,
         logoShape: logoShape.logoShape,
         shapeColor: shapeColor.shapeColor
     }
 
-    if (userInputs.logoShape === "circle") {
-        const logo = new Circle(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);
-        logo.render();
-        logo.createSVG();
-    } else if (userInputs.logoShape === "triangle") {
-        const logo = new Triangle(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);
-        logo.render();
-        logo.createSVG();
-    } else {
-        const logo = new Square(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);
-        logo.render();
-        logo.createSVG();
+    if (userInputs.logoShape === "circle") {                                                                                    // IF the user picked for their shape to be a circle
+        const logo = new Circle(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);        // THEN a new Circle is created
+        logo.render();                                                                                                          // THEN the SVG file text is rendered
+        logo.createSVG();                                                                                                       // THEN the SVG is created
+    } else if (userInputs.logoShape === "triangle") {                                                                           // IF the user picked for their shape to be a triangle
+        const logo = new Triangle(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);      // THEN a new Triangle is created
+        logo.render();                                                                                                          // THEN the SVG file text is rendered
+        logo.createSVG();                                                                                                       // THEN the SVG is created
+    } else {                                                                                                                    // IF the user picked for their shape to be a square
+        const logo = new Square(userInputs.logoText, userInputs.textColor, userInputs.logoShape, userInputs.shapeColor);        // THEN a new Square is created
+        logo.render();                                                                                                          // THEN the SVG file text is rendered
+        logo.createSVG();                                                                                                       // THEN the SVG is created
     }
 }
 
+// initializes the prompting when the file is run
 handlePrompting();
